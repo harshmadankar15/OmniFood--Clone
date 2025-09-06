01e684f4e65ab390ac07811865e31eb834c5d275
Harsh Madankar- ThinkerSteps <harsh.madankar@thinkersteps.com>
2025-09-06 14:47:50 +0530

    Initial commit of OmniFood-Clone

---
+++ main.js
@@ -0,0 +1,10 @@
+// MAKE MOBILE NAVIGATION WORK
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+    headerEl.classList.toggle("nav-open");
+
+});
